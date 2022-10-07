1b9879bca6d8868b202d0159547ce018a0b99ad6
Pruthwik <pruthwikmishra@gmail.com>
2022-10-08 00:28:39 +0530

    New-Tokenizer

--- tokenizer_for_all_indian_languages_in_raw_format.py
+++ tokenizer_for_all_indian_languages_in_raw_format.py
@@ -1,15 +1,20 @@
 # how to run the code
-# python3 tokenizer_for_file.py --input Input --output Output
+# python3 tokenizer_for_all_indian_languages_in_raw_format.py --input Input --output Output --lang lang
 # works at folder and file levels
 # lang = 0 for languages ['hi', 'or', 'mn', 'as', 'bn', 'pa'], purna biram as sentence end marker
-# lang = 1 for Urdu '۔' sentence end marker
+# lang = 1 for Urdu and Kashmiri '۔' sentence end marker
 # lang = 2 for languages ['en', 'gu', 'mr', 'ml', 'kn', 'te', 'ta'] '.' sentence end marker
+# the mapping of languages and ISO code
+# Hindi: hi, Odia: or, Manipuri: mn, Assamese: as, Bengali: bn, Punjabi: pa
+# Urdu: ur, Kashmiri: ks
+# English: en, Gujarati: gu, Marathi: mr, Malayalam: ml, Kannada: kn, Telugu: te, Tamil: ta
 import re
 import argparse
 import os
 from string import punctuation
 
 
+# the below code defines different kinds of regular expressions
 token_specification = [
     ('datemonth',
      r'^(0?[1-9]|1[012])[-\/\.](0?[1-9]|[12][0-9]|3[01])[-\/\.](1|2)\d\d\d$'),
@@ -36,12 +41,14 @@ token_specification = [
     ('quotes_question', r'[”\?]'),
     ('hashtag', r'#')
 ]
+# the below code converts the above expression into a python regex
 tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
 get_token = re.compile(tok_regex)
 punctuations = punctuation + '\"\'‘’“”'
 
 
 def tokenize(list_s):
+    """Tokenize a list of tokens."""
     tkns = []
     for wrds in list_s:
         wrds_len = len(wrds)
@@ -71,54 +78,63 @@ def tokenize(list_s):
     return tkns
 
 
+def read_lines_from_file(file_path):
+    """Read lines from a file."""
+    with open(file_path, 'r', encoding='utf-8') as file_read:
+        return [line.strip() for line in file_read.readlines() if line.strip()]
+
+
 def read_file_and_tokenize(input_file, lang_type=0):
-        file_read = open(input_file, 'r', encoding='utf-8')
-        text = file_read.read().strip()
-        if lang_type == 0:
-            sentences = re.findall('.*?।|.*?\n', text + '\n', re.UNICODE)
-            endMarkers = ['?', '।', '!', '|']
-        elif lang_type == 1:
-            sentences = re.findall('.*?\n', text + '\n', re.UNICODE)
-            endMarkers = ['؟', '!', '|', '۔']
-        else:
-            sentences = re.findall('.*?\n', text + '\n', re.UNICODE)
-            endMarkers = ['?', '.', '!', '|']
-        proper_sentences = list()
-        print(endMarkers, lang_type)
-        for index, sentence in enumerate(sentences):
-            if sentence.strip() != '':
-                list_tokens = tokenize(sentence.split())
-                end_sentence_markers = [index + 1 for index, token in enumerate(list_tokens) if token in endMarkers]
-                if len(end_sentence_markers) > 0:
-                    if end_sentence_markers[-1] != len(list_tokens):
-                        end_sentence_markers += [len(list_tokens)]
-                    end_sentence_markers_with_sentence_end_positions = [0] + end_sentence_markers
-                    sentence_boundaries = list(zip(end_sentence_markers_with_sentence_end_positions, end_sentence_markers_with_sentence_end_positions[1:]))
-                    for start, end in sentence_boundaries:
-                        individual_sentence = list_tokens[start: end]
-                        proper_sentences.append(' '.join(individual_sentence))
-                else:
-                    proper_sentences.append(' '.join(list_tokens))
-                if index < len(sentences) - 1:
-                    nextSentence = sentences[index + 1]
-                    nextTokens = tokenize(nextSentence.split())
-                    punctFlag = True
-                    for token in nextTokens:
-                        punctFlag &= token in punctuations
-                    if punctFlag:
-                        print(nextTokens)
-                        if proper_sentences:
-                            proper_sentences[-1] += ' ' + ' '.join(nextTokens)
-                            sentences[index + 1] = ''
-        return proper_sentences
+    """Read a file and tokenize its content by specifying the input file path and language type."""
+    file_read = open(input_file, 'r', encoding='utf-8')
+    lines = read_lines_from_file(input_file)
+    text = '\n'.join(lines)
+    if lang_type == 0:
+        sentences = re.findall('.*?।|.*?\n', text + '\n', re.UNICODE)
+        end_markers = ['?', '।', '!', '|']
+    elif lang_type == 1:
+        sentences = re.findall('.*?\n', text + '\n', re.UNICODE)
+        end_markers = ['؟', '!', '|', '۔']
+    else:
+        sentences = re.findall('.*?\n', text + '\n', re.UNICODE)
+        end_markers = ['?', '.', '!', '|']
+    proper_sentences = list()
+    for index, sentence in enumerate(sentences):
+        sentence = sentence.strip()
+        if sentence != '':
+            list_tokens = tokenize(sentence.split())
+            end_sentence_markers = [index + 1 for index, token in enumerate(list_tokens) if token in end_markers]
+            if len(end_sentence_markers) > 0:
+                if end_sentence_markers[-1] != len(list_tokens):
+                    end_sentence_markers += [len(list_tokens)]
+                end_sentence_markers_with_sentence_end_positions = [0] + end_sentence_markers
+                sentence_boundaries = list(zip(end_sentence_markers_with_sentence_end_positions, end_sentence_markers_with_sentence_end_positions[1:]))
+                for start, end in sentence_boundaries:
+                    individual_sentence = list_tokens[start: end]
+                    proper_sentences.append(' '.join(individual_sentence))
+            else:
+                proper_sentences.append(' '.join(list_tokens))
+            if index < len(sentences) - 1:
+                next_sentence = sentences[index + 1]
+                next_tokens = tokenize(next_sentence.split())
+                punct_flag = True
+                for token in next_tokens:
+                    punct_flag &= token in punctuations
+                if punct_flag:
+                    if proper_sentences:
+                        proper_sentences[-1] += ' ' + ' '.join(next_tokens)
+                        sentences[index + 1] = ''
+    return proper_sentences
 
 
 def write_list_to_file(output_file, data_list):
+    """Write a list to a file."""
     with open(output_file, 'w', encoding='utf-8') as file_write:
         file_write.write('\n'.join(data_list) + '\n')
 
 
-if __name__ == '__main__':
+def main():
+    """Pass arguments and call functions here."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--input', dest='inp', help="enter the input file path")
@@ -129,26 +145,30 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if os.path.isdir(args.inp) and not os.path.isdir(args.out):
         os.mkdir(args.out)
-    langCode = args.lang
+    lang_code = args.lang
     if not os.path.isdir(args.inp):
-        if langCode in ['hi', 'or', 'mn', 'as', 'bn', 'pa']:
+        if lang_code in ['hi', 'or', 'mn', 'as', 'bn', 'pa']:
             lang = 0
-        elif langCode == 'ur':
+        elif lang_code in ['ur', 'ks']:
             lang = 1
-        elif langCode in ['en', 'gu', 'mr', 'ml', 'kn', 'te', 'ta']:
+        elif lang_code in ['en', 'gu', 'mr', 'ml', 'kn', 'te', 'ta']:
             lang = 2
         sentences = read_file_and_tokenize(args.inp, lang)
         write_list_to_file(args.out, sentences)
     else:
         for root, dirs, files in os.walk(args.inp):
             for fl in files:
-                inputFilePath = os.path.join(root, fl)
-                if langCode in ['hi', 'or', 'mn', 'as', 'bn', 'pa']:
+                input_file_path = os.path.join(root, fl)
+                if lang_code in ['hi', 'or', 'mn', 'as', 'bn', 'pa']:
                     lang = 0
-                elif langCode == 'ur':
+                elif lang_code in ['ur', 'ks']:
                     lang = 1
-                elif langCode in ['en', 'gu', 'mr', 'ml', 'kn', 'te', 'ta']:
+                elif lang_code in ['en', 'gu', 'mr', 'ml', 'kn', 'te', 'ta']:
                     lang = 2
-                sentences = read_file_and_tokenize(inputFilePath, lang)
-                outputFilePath = os.path.join(args.out, fl)
-                write_list_to_file(outputFilePath, sentences)
+                sentences = read_file_and_tokenize(input_file_path, lang)
+                output_file_path = os.path.join(args.out, fl)
+                write_list_to_file(output_file_path, sentences)
+
+
+if __name__ == '__main__':
+    main()
